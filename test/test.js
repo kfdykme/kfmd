41ab390a60b6a3c9dce875419bea8fd8fa4d9b3d
kfdykme <wim.k.f@live.com>
2019-02-13 20:32:52 +0800

    [Update]

--- test/test.js
+++ test/test.js
@@ -4,5 +4,4 @@ const fs = require('fs')
 console.info(fs.readFileSync)
 var result = fs.readFileSync('./test/maxiang.md') + ''
 var html = kfmd.string2mdhtml(result)
-
 console.info(fs.writeFileSync('./index.html', html))
